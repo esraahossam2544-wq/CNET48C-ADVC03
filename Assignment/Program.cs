6f29ca82d9eed7935b710923ddd607a9e759c122
Esraa Hussam <Esraa Hussam@DESKTOP-AMU5C4M>
2026-09-05 09:55:05 +0300

    Ex01

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+#region Ex01
+
+List<int> grades = new List<int> { 85, 92, 78, 95, 88, 70, 100, 65 };
+
+Console.WriteLine("Grades: " + string.Join(", ", grades));
+Console.WriteLine("Count: " + grades.Count);
+Console.WriteLine("First: " + grades.First());
+Console.WriteLine("Last: " + grades.Last());
+grades.Sort();
+Console.WriteLine("Sorted Ascending: " + string.Join(", ", grades));
+int firstAbove90 = grades.First(g => g > 90);
+Console.WriteLine("First grade above 90: " + firstAbove90);
+List<int> failingGrades = grades.Where(g => g < 75).ToList();
+Console.WriteLine("Failing grades: " + string.Join(", ", failingGrades));
+grades.RemoveAll(g => g < 75);
+Console.WriteLine("After removing failing grades: " + string.Join(", ", grades));
+bool hasPerfectScore = grades.Any(g => g == 100);
+Console.WriteLine("Contains a 100: " + hasPerfectScore);
+List<string> formattedGrades = grades.Select(g => $"Grade: {g}").ToList();
+Console.WriteLine("Formatted:");
+foreach (var g in formattedGrades)
+    Console.WriteLine(g);
+#endregion
